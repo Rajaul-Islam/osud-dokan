332d1f905f4b45d5020f023799fcd624f91322d7
rajaul rabbi <Rajaul-Islam>
2021-10-18 14:21:46 +0600

    first commit

--- src/Hook/useFirebase.js
+++ src/Hook/useFirebase.js
@@ -1,5 +1,5 @@
 import firebaseInitialize from "../firebase/firebase.init";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut,onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { useEffect, useState } from "react";
 firebaseInitialize();
 
@@ -11,6 +11,10 @@ const GoogleProvider = new GoogleAuthProvider();
 const useFirebase = () => {
     const [user, setUser] = useState({})
     const [error, setError] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState("")
+
+    // google signup
     const signInWithGoogle = () => {
         signInWithPopup(auth, GoogleProvider)
             .then(result => {
@@ -23,18 +27,64 @@ const useFirebase = () => {
             })
 
     }
-    useEffect(()=>{
-        const unsubscribed= onAuthStateChanged(auth, (user) => {
+    // username and password 
+    const signInWithEmail = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                setUser(result.user)
+                console.log(result.user);
+                alert('loged in successfully')
+
+            })
+            .catch((error) => {
+                setError(error.message)
+            })
+
+    }
+
+    // get email 
+    const getEmail = e => {
+        const text=e?.target?.value
+        setEmail(text);
+        console.log(email)
+    }
+
+    //get password
+    const getPassword = e => {
+        const pass=e?.target?.value;
+        setPassword(pass);
+        console.log(password)
+    }
+
+
+    useEffect(() => {
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
             if (user) {
-             setUser(user)
-            
+                setUser(user)
+
             } else {
-               
-             setUser({})
+
+                setUser({})
             }
-          });
-          return ()=> unsubscribed;
-    },[])
+        });
+        return () => unsubscribed;
+    }, [])
+
+    const signUp = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+
+                alert('user created')
+
+            })
+            .catch((error) => {
+                setError(error.message);
+
+            });
+
+    }
 
     const logOut = () => {
         const auth = getAuth();
@@ -49,7 +99,12 @@ const useFirebase = () => {
         signInWithGoogle,
         user,
         error,
-        logOut
+        logOut,
+        signInWithEmail,
+        getEmail,
+        getPassword,
+        signUp
+
 
     };
 };
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,12 +1,79 @@
+import { faEnvelope, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer
-                
-            </h1>
+        <div className="text-white">
+        <div className="py-4 gradient">
+          <Container>
+            <Row>
+              <Col md={6}>
+                <div className="text-center my-2">
+                  {/* <img width="120px" src={footerLogo} alt="" /> */}
+                  <h1>this is an image</h1>
+                </div>
+  
+                <ul className="list-unstyled">
+                  <li>
+                    <FontAwesomeIcon icon={faMapMarkerAlt} />
+                    <span className="ms-1 fs-5">Naogaon,Rajshahi,Bangladesh</span>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <span className="ms-1 fs-5">
+                      Official: coding.club.pro@gmail.com
+                    </span>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faPhone} />
+                    <span className="ms-1 fs-5">
+                      Helpline: 01763251119(Available:10:00AM to 10.00PM)
+                    </span>
+                  </li>
+                </ul>
+              </Col>
+              <Col md={2}>
+                <ul className="list-unstyled footer-link">
+                  <li>
+                    <NavLink to="/home">Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/about">About us</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact">Contact us</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/courses">Courses</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/policy">Policy</NavLink>
+                  </li>
+                </ul>
+              </Col>
+              <Col md={4}>
+                <div className="">
+                  {/* <img
+                    className="img-fluid"
+                    src={payment}
+                    alt="payment methods"
+                  /> */}
+                  <h1>Tshi is on image</h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
+        <hr className="m-0 p-0" />
+        <p className="text-center m-0 py-3 copyright">
+          Copyright © All Reserved by Coding Club pro - programming Community in
+          2021
+        </p>
+      </div>
     );
 };
 
